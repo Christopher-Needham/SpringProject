38b63989a906cd0ab3770ebd65769e40f13c0737
Christopher Needham <christopher.needham09@gmail.com>
2022-12-18 22:02:45 -0600

    first commit

---
+++ src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.entity;
+
+/**
+ * @author 17015
+ *
+ */
+public enum JeepModel {
+    WRANGLER, GRAND_CHEROKEE, CHEROKEE, COMPASS, RENEGADE, GLADIATOR, WRANGLER_4XE
+
+  
+
+
+}
